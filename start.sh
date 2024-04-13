80d8e3d6056e18f75366af8f17a8f8144a5aba2d
Brody <brodyover@gmail.com>
2024-04-13 02:28:20 -0400

    updates

--- start.sh
+++ start.sh
@@ -3,18 +3,18 @@
 set -e
 
 if [ -z "$RAILWAY_VOLUME_MOUNT_PATH" ]; then
-    echo no volume attached, please attach a volume.
-    echo upon attaching a volume let this service rebuild
+    echo "no volume attached, please attach a volume"
+    echo "upon attaching a volume let this service rebuild"
     exit 0
 fi
 
 if [ -z "$WEB_USERNAME" ]; then
-    echo missing the WEB_USERNAME variable, please add it to continue
+    echo "missing the WEB_USERNAME variable, please add it to continue"
     exit 0
 fi
 
 if [ -z "$WEB_PASSWORD" ]; then
-    echo missing the WEB_PASSWORD variable, please add it to continue
+    echo "missing the WEB_PASSWORD variable, please add it to continue"
     exit 0
 fi
 
@@ -29,7 +29,7 @@ DATABASE_PATH=$FILEBROWSER_DATA_PATH/filebrowser.db
 FILEBROWSER_USERNAME_PATH=$FILEBROWSER_DATA_PATH/username
 
 if [ "$USE_VOLUME_ROOT" == "1" ]; then
-    echo using volume root as storage location
+    echo "using volume root as storage location"
     FILEBROWSER_STORAGE_PATH=$RAILWAY_VOLUME_MOUNT_PATH
 else
     FILEBROWSER_STORAGE_PATH=$RAILWAY_VOLUME_MOUNT_PATH/storage
@@ -43,28 +43,28 @@ if [ -f "$DATABASE_PATH" ]; then
         FILEBROWSER_CURRENT_USERNAME=$(cat $FILEBROWSER_USERNAME_PATH)
 
         if [[ -n "$FILEBROWSER_CURRENT_USERNAME" && "$FILEBROWSER_CURRENT_USERNAME" != "$WEB_USERNAME" ]]; then
-            echo new username was set in the service variables, changing username: $FILEBROWSER_CURRENT_USERNAME "->" $WEB_USERNAME
-            /filebrowser users update $FILEBROWSER_CURRENT_USERNAME --username $WEB_USERNAME --database $DATABASE_PATH > /dev/null
+            echo "new username was set in the service variables, changing username: $FILEBROWSER_CURRENT_USERNAME -> $WEB_USERNAME"
+            /filebrowser users update $FILEBROWSER_CURRENT_USERNAME --username $WEB_USERNAME --database $DATABASE_PATH > /dev/null 2>&1
             echo $WEB_USERNAME >| $FILEBROWSER_USERNAME_PATH
-            echo username updated
+            echo "username updated"
         fi
     fi
 else
-    echo first start, creating database
-    /filebrowser config init --database $DATABASE_PATH > /dev/null
+    echo "first start, creating database"
+    /filebrowser config init --database $DATABASE_PATH > /dev/null 2>&1
 
-    echo setting configurations
-    /filebrowser config set --address "0.0.0.0" --database $DATABASE_PATH > /dev/null
+    echo "setting configurations"
+    /filebrowser config set --address "0.0.0.0" --database $DATABASE_PATH > /dev/null 2>&1
 
-    /filebrowser users add $WEB_USERNAME $WEB_PASSWORD --database $DATABASE_PATH > /dev/null
+    /filebrowser users add $WEB_USERNAME $WEB_PASSWORD --database $DATABASE_PATH > /dev/null 2>&1
 fi
 
 echo $WEB_USERNAME > $FILEBROWSER_USERNAME_PATH
 
-/filebrowser users update $WEB_USERNAME --password $WEB_PASSWORD --database $DATABASE_PATH > /dev/null
+/filebrowser users update $WEB_USERNAME --password $WEB_PASSWORD --database $DATABASE_PATH > /dev/null 2>&1
 
-/filebrowser config set --port $PORT --database $DATABASE_PATH > /dev/null
-/filebrowser config set --root $FILEBROWSER_STORAGE_PATH --database $DATABASE_PATH > /dev/null
+/filebrowser config set --port $PORT --database $DATABASE_PATH > /dev/null 2>&1
+/filebrowser config set --root $FILEBROWSER_STORAGE_PATH --database $DATABASE_PATH > /dev/null 2>&1
 
 /filebrowser version
 
